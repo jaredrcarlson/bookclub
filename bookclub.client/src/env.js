d6a83c5e16639eddeb0dfb9e2ff943cab99b385f
Jared Carlson <jaredrcarlson@gmail.com>
2023-07-31 12:27:58 -0600

    env.js updated

--- bookclub.client/src/env.js
+++ bookclub.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
+export const audience = 'https://localhost:8888'
+export const clientId = '7TkwLlEwyOaG8jqhrymJz1GHkaAVsCdx'
