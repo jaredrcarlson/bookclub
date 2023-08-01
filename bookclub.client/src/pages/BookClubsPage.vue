b2ccd4393ea806cdaa1eab7db5265d09f5c01306
Becca <becca.vandewater@gmail.com>
2023-08-01 16:46:25 -0600

    Home page styling started

---
+++ bookclub.client/src/pages/BookClubsPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-12">
+        <h1>
+          Book Clubs Page
+        </h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- bookclub.client/src/pages/HomePage.vue
+++ bookclub.client/src/pages/HomePage.vue
@@ -1,12 +1,107 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container-fluid">
+    <section class="row dark-blue-bg text-light">
+      <div class="col-12 text-center mt-5 mb-3">
+
+        <div class="mb-4">
+          <h3 class="fw-bold">
+            Find a <span class="orange-text">book club</span> right for you
+          </h3>
+          <h4>
+            Discuss, rate, and enjoy the books you love with a community you create.
+          </h4>
+        </div>
+
+        <div>
+          <button class="btn orange-btn m-2">
+            Find a Book Club
+          </button>
+          <button class="btn light-blue-btn m-2">
+            Make a Book Club
+          </button>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="row">
+
+      <div class="col-12 d-flex p-0">
+        <img class="img-fluid home-banner flex-fill" src="https://images.unsplash.com/photo-1618414952052-289dab88b409?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="book shelf">
+      </div>
+
+    </section>
+
+    <section class="row ghost-bg">
+      <div class="col-12 mt-5">
+        <h6>
+          NY Times Best Sellers
+        </h6>
+      </div>
+    </section>
+
+    <section class="row ghost-bg">
+      <div class="col-12 mt-5">
+        <section class="row">
+
+          <div class="col-6">
+            <p class="fs-6 fw-medium">
+              Bookclub List
+            </p>
+          </div>
+
+          <div class="col-6 d-flex justify-content-end">
+            <div class="me-3">
+              <button class="btn orange-btn">
+                Create a Club
+              </button>
+            </div>
+
+            <div>
+              <form>
+                <div class="d-flex">
+                  <label for="search"></label>
+                  <input type="text" name="search" id="search" placeholder="Search books..." class="form-control">
+                  <button class="btn orange-btn" title="Submit" type="submit">
+                    <i class="mdi mdi-magnify"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+
+          </div>
+        </section>
+      </div>
+    </section>
+
+    
+
+    <section class="row ghost-bg">
+
+      <!-- FIXME Turn this section into a v-for over a club card component -->
+      <div class="col-md-6 col-12 mb-3">
+        <div>
+          <img class="img-fluid card-img" src="https://images.unsplash.com/photo-1553434320-e9f5757140b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80" alt="card img">
+        </div>
+        <div class="dark-blue-bg p-3 text-light">
+          <p class="fs-5">
+            The Fantasy Book Club
+          </p>
+          <p>
+            Exploring new worlds and new lands! Always looking for new members!
+          </p>
+          <div class="d-flex justify-content-between">
+            <span>
+              25 club members
+            </span>
+            <button class="btn orange-btn">
+              Join Club
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
@@ -19,23 +114,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
 
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.home-banner{
+  height: 30vh;
+  object-fit: cover;
+  object-position: center;
 }
+
+.card-img{
+  height:50vh;
+  object-fit: cover;
+  object-position: center;
+}
+
 </style>
