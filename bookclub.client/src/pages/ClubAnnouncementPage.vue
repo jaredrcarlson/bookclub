10d27be97fb568ac7f84d59bd03209d5b7310f72
Becca <becca.vandewater@gmail.com>
2023-08-02 15:00:26 -0600

    sync test

--- bookclub.client/src/pages/ClubAnnouncementPage.vue
+++ bookclub.client/src/pages/ClubAnnouncementPage.vue
@@ -9,27 +9,27 @@
     </section>
     <section class="row">
       <!-- FIXME Make this into a v-for that pulls up all announcements with their title, creator, createdAt, and commentCount Put into a component. -->
-      <div class="col-12 border border-black elevation-2 mb-3 rounded">
+      <div class="col-12 border border-black elevation-2 mb-3 rounded bg-light">
         <router-link :to="{name: 'Announcement Details Page', params:{announcementId: 'announcement'}}">
-        <section class="row align-items-center p-2">
-          <div class="col-md-2 col-12">
-            <img class="img-fluid avatar-img" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="">
-          </div>
-          <div class="col-md-8 col-12">
-            <p class="fw-bold fs-3">
-              Reading Chapters 1-3 by Friday!
-            </p>
-            <p>
-              <span>BookDiver232 </span> <span>posted 12/16/2023</span>
-            </p>
-          </div>
-          <div class="col-md-2 col-12">
-            <p class="fs-5">
-              <i class="mdi mdi-message-reply"></i> <span>10</span>
-            </p>
-          </div>
-        </section>
-      </router-link>
+          <section class="row align-items-center p-2 text-dark">
+            <div class="col-md-2 col-12">
+              <img class="img-fluid avatar-img" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="">
+            </div>
+            <div class="col-md-8 col-12">
+              <p class="fw-bold fs-3">
+                Reading Chapters 1-3 by Friday!
+              </p>
+              <p>
+                <span class="pe-4">BookDiver232</span><span>posted 12/16/2023</span>
+              </p>
+            </div>
+            <div class="col-md-2 col-12">
+              <p class="fs-5">
+                <i class="mdi mdi-message-reply"></i> <span>10</span>
+              </p>
+            </div>
+          </section>
+        </router-link>
       </div>
     </section>
   </div>
