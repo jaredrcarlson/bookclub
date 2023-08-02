1d99e583ca3bdb74f17e5e35faaf13cce82c49b2
Bailey Johnson <bailey91716@gmail.com>
2023-08-02 10:37:07 -0600

    server: delete members, get members by club id

--- bookclub/server/controllers/ClubsController.js
+++ bookclub/server/controllers/ClubsController.js
@@ -1,6 +1,7 @@
 import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 import { clubsService } from "../services/ClubsService.js";
+import { clubMembersService } from "../services/ClubMembersService.js";
 
 export class ClubsController extends BaseController {
 
@@ -10,10 +11,13 @@ export class ClubsController extends BaseController {
       //routes
       .get('', this.getClubs)
       .get('/:clubId', this.getClubById)
+      .get('/:clubId/members', this.getClubMembers)
 
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createClub)
+      .put('/:clubId', this.updateClub)
   }
+  /** This function gets/reads ALL of the clubs */
   async getClubs(req, res, next) {
     try {
       const clubs = await clubsService.getClubs()
@@ -22,6 +26,7 @@ export class ClubsController extends BaseController {
       next(error)
     }
   }
+  /**this function gets ONE club  */
   async getClubById(req, res, next) {
     try {
       const clubId = req.params.clubId
@@ -31,6 +36,17 @@ export class ClubsController extends BaseController {
       next(error)
     }
   }
+  /* This function gets all the members that are within a club by its id. It READS the members.  */
+  async getClubMembers(req, res, next) {
+    try {
+      const clubId = req.params.clubId
+      const clubMembers = await clubMembersService.getClubMembers(clubId)
+      return res.send(clubMembers)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**this function allows a user to create a book club, and assigns them as the 'creator' */
   async createClub(req, res, next) {
     try {
       const clubData = req.body
@@ -39,6 +55,13 @@ export class ClubsController extends BaseController {
       return res.send(club)
     } catch (error) {
       next(error)
+    }
+  }
+  async updateClub(req, res, next) {
+    try {
+
+    } catch (error) {
+
     }
   }
 }
