8ec48fa901ce363ab64cf5b018e637615582d5a2
Jared Carlson <jaredrcarlson@gmail.com>
2023-08-02 16:53:25 -0600

    created UserBook enpoints (untested)

--- bookclub/server/controllers/AccountController.js
+++ bookclub/server/controllers/AccountController.js
@@ -1,6 +1,7 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
 import BaseController from '../utils/BaseController'
+import { userBooksService } from '../services/UserBooksService.js'
 
 export class AccountController extends BaseController {
   constructor() {
@@ -8,12 +9,23 @@ export class AccountController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
+      .get('', this.getUserBooks)
   }
 
   async getUserAccount(req, res, next) {
     try {
       const account = await accountService.getAccount(req.userInfo)
       res.send(account)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getUserBooks(req, res, next) {
+    try {
+      const userId = req.userInfo.id
+      const userBooks = await userBooksService.getUserBooksByUserId(userId)
+      return res.send(userBooks)
     } catch (error) {
       next(error)
     }
