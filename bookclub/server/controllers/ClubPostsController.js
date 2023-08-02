695128c7a6426bbf874c92f52da6a39a3c9b8cf9
Bailey Johnson <bailey91716@gmail.com>
2023-08-02 14:38:25 -0600

    server: create and get posts

--- bookclub/server/controllers/ClubPostsController.js
+++ bookclub/server/controllers/ClubPostsController.js
@@ -9,12 +9,26 @@ export class ClubPostsController extends BaseController {
       //routes
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('/:postId', this.createPost)
+      .put('/:postId', this.updatePost)
   }
+  /**Function that allows a user to create a post on a specific club */
   async createPost(req, res, next) {
     try {
       const postData = req.body
       postData.creatorId = req.userInfo.id
       const post = await clubPostsService.createPost(postData)
+      return res.send(post)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**Function that let's a user update their own post, but not another user's */
+  async updatePost(req, res, next) {
+    try {
+      const postId = req.params.postId
+      const userId = req.userInfo.id
+      const postData = req.body
+      const post = await clubPostsService.updatePost(postId, userId, postData)
       return res.send(post)
     } catch (error) {
       next(error)
