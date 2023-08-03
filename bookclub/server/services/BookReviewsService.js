4a3a9604a2ab5e1c577e4b27ed311b4028702ee5
Arrington Mata <mataarrington@gmail.com>
2023-08-03 12:16:36 -0600

    CRUD for BookReviews

--- bookclub/server/services/BookReviewsService.js
+++ bookclub/server/services/BookReviewsService.js
@@ -1,5 +1,45 @@
+import { dbContext } from "../db/DbContext"
+import { BadRequest, Forbidden } from "../utils/Errors"
+
 class BookReviewsService {
-    async createReview
+
+    async getReviewByGbid(gbId) {
+        const reviews = await dbContext.BookReviews.find({ gbId })
+        return reviews
+    }
+
+    async editReview(reviewId, reqBody) {
+        let review = await this.getReviewById(reviewId)
+        if (review.creatorId != reqBody.creatorId) {
+            throw new Forbidden('This review does not belong to you.')
+        }
+        review.content = reqBody.content || review.content
+        review.rating = reqBody.rating || review.rating
+        await review.save()
+        return review
+    }
+
+    async deleteReview(reviewId, reqBody) {
+        let review = await this.getReviewById(reviewId)
+        if (review.creatorId != reqBody.creatorId) {
+            throw new Forbidden('This review does not belong to you.')
+        }
+        await review.remove()
+        return review
+    }
+
+    async createReview(reviewBody) {
+        const review = await dbContext.BookReviews.create(reviewBody)
+        await review.populate('creator', 'name picture')
+        return review
+    }
+    async getReviewById(reviewId) {
+        const review = await dbContext.BookReviews.findById(reviewId).populate('creator', 'name picture')
+        if (!review) {
+            throw new BadRequest(`There is no such review with the ID: ${reviewId}`)
+        }
+        return review
+    }
 }
 
 export const bookReviewsService = new BookReviewsService()
