1d8af5d88863bf75b34ada0ec62b153654177d4b
Bailey Johnson <bailey91716@gmail.com>
2023-08-04 14:52:25 -0600

    Get,Create,Delete Announcements

--- bookclub.client/src/components/AnnouncementCard.vue
+++ bookclub.client/src/components/AnnouncementCard.vue
@@ -1,52 +1,55 @@
 <template>
-  <section title="Go to this Post and its Comments" class="row align-items-center p-2 text-dark selectable ">
-        <div class="col-md-2 col-12">
-          <img class="img-fluid avatar-img" :src="postProp?.creator?.picture" :title="postProp.creator?.name" :alt="postProp?.creator?.name">
-        </div>
-        <div class=" col-md-8 col-12">
-              
-              <router-link :to="{name: 'Discussion Details Page', params:{postId: postProp.id}}">
-              <p class="selectable fw-bold fs-3 text-dark">
-                {{postProp?.title}}
+  <router-link :to="{name: 'Announcement Details Page', params:{announcementId: 'announcement'}}">
+          <section class="row align-items-center p-2 text-dark">
+            <div class="col-md-2 col-12">
+              <img class="img-fluid avatar-img" :src="announcementProp.creator?.picture" :alt="announcementProp.creator?.name">
+            </div>
+            <div class="col-md-8 col-12">
+              <p class="fw-bold fs-3">
+                {{ announcementProp.title }}
               </p>
-            </router-link>
+              <p class="fw-bold fs-4">
+                {{ announcementProp.body }}
+              </p>
               <p>
-                <span class="pe-4">{{postProp?.creator?.name}}</span><span>posted {{postProp?.createdAt}}</span>
+                <span class="pe-4">{{announcementProp.creator?.name}}</span><span>posted {{announcementProp.createdAt}}</span>
               </p>
             </div>
-            <div class="col-md-2 col-12  justify-content-evenly">
-              <div v-if="postProp?.creatorId == account?.id" class="btn-group align-items-start mb-3">
+            <div class="col-md-2 col-12">
+              <div v-if="announcementProp?.creatorId == account?.id" class="btn-group align-items-start mb-3">
                 <button title="More Options" class="btn orange-text text-end btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
         <i class="mdi mdi-circle"></i><i class="mdi mdi-circle"></i><i class="mdi mdi-circle"></i>
         </button>
         <ul  class="p-1 dropdown-menu">
-        <li @click="deletePost(postProp?.id)" class="selectable mb-1 p-1">
-        Delete Comment <i class="mdi mdi-delete"></i>
+        <li @click="deletePost(announcementProp?.id)" class="selectable mb-1 p-1">
+        Delete Announcement <i class="mdi mdi-delete"></i>
         </li>
         <li class="selectable mb-1 p-1">
-        Edit Comment <i class="mdi mdi-pencil"></i>
+        Edit Announcement <i class="mdi mdi-pencil"></i>
       </li>
 
     </ul>
   </div>
-  <p class="fs-5 ">
-    <i title="See Comments on this Post" class="mdi mdi-message-reply"></i> <span>1</span>
-  </p>
+              <p class="fs-5">
+                <i class="mdi mdi-message-reply"></i> <span>{{announcementProp.commentCount}}10</span>
+              </p>
             </div>
+            
           </section>
+        </router-link>
 </template>
 
 
 <script>
 import { computed } from "vue";
 import { AppState } from "../AppState.js";
+import Pop from "../utils/Pop.js";
 import { logger } from "../utils/Logger.js";
 import { clubPostsService } from "../services/ClubPostsService.js";
-import Pop from "../utils/Pop.js";
 
 export default {
   props: {
-    postProp: { type: Object, required: true}
+    announcementProp: { type: Object, required: true }
   },
   setup(){
     return {
@@ -64,7 +67,6 @@ export default {
                     Pop.error(error.message);
                 }
             },
-
     }
   }
 }
@@ -73,11 +75,10 @@ export default {
 
 <style lang="scss" scoped>
 .avatar-img{
+  height: 10vh;
+  width: 10vh;
   border-radius: 50%;
   object-fit: cover;
   object-position: center;
-  height: 10vh;
-  width: 10vh;
-  box-shadow: 3px 3px 2px black;
 }
 </style>
