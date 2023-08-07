decc099e3ce0869bb4b0559b3b35952b079a815c
Becca <becca.vandewater@gmail.com>
2023-08-07 15:16:28 -0600

    Booklist cards mostly nice looking

--- bookclub.client/src/pages/ClubListPage.vue
+++ bookclub.client/src/pages/ClubListPage.vue
@@ -1,41 +1,49 @@
 <template>
-  <div>
-    <div>
-      <p class="fs-1 fw-semibold mt-2">
-        Club's Booklist
-      </p>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <p class="fs-1 fw-semibold mt-2">
+          Club's Booklist
+        </p>
+      </div>
     </div>
-    <div class="table-responsive">
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col" class="fw-light">Title</th>
-            <th scope="col" class="fw-light">Progress</th>
-            <th scope="col" class="fw-light">Rating</th>
-          </tr>
-        </thead>
-        <tbody class="fw-semibold">
-          <!-- FIXME v-for all of the books in a list. Convert the table row into a card component. Add a router link on each row to go to that book's detail page. -->
-          <tr scope="row">
-            
-          </tr>
-        </tbody>
-      </table>
+    <div class="row">
+      <div class="col-12 my-3">
+        <BookListCard />
+      </div>
     </div>
   </div>
 </template>
 
 
 <script>
-import { computed } from 'vue';
+import { computed, watchEffect } from 'vue';
 import { AppState } from '../AppState.js';
+import { useRoute } from 'vue-router';
+import Pop from '../utils/Pop.js';
+import { booksService } from '../services/BooksService.js';
 
 export default {
-  setup(){
-    return {
-      selectedClub: computed(() => AppState.selectedClub)
-    }
-  }
+    setup() {
+        const route = useRoute();
+        async function getBooksByClubId() {
+            try {
+                const clubId = route.params.clubId;
+                await booksService.getBooksByClubId(clubId);
+            }
+            catch (error) {
+                Pop.error(error.message);
+            }
+        }
+        watchEffect(() => {
+            getBooksByClubId();
+        });
+        return {
+            route,
+            selectedClub: computed(() => AppState.selectedClub),
+            books: computed(() => AppState.books)
+        };
+    },
 }
 </script>
 
