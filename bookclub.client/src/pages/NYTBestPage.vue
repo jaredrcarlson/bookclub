a5f938fd1f86600ebd0877772e33c687329548a1
Becca <becca.vandewater@gmail.com>
2023-08-08 16:38:47 -0600

    fix for nytimes list on home page?

---
+++ bookclub.client/src/pages/NYTBestPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Books Page</h1>
+</template>
+
+
+<script>
+import { computed, onMounted } from 'vue'
+import { newYorkTimesService } from '../services/NewYorkTimesService.js'
+import Pop from '../utils/Pop.js'
+import { AppState } from '../AppState.js'
+
+export default {
+  setup(){
+    async function getTopBooks(){
+      try {
+        await newYorkTimesService.getTopBooks()
+      } catch (error) {
+        Pop.error(error.message)
+      }
+    }
+
+    onMounted(()=> {
+      getTopBooks()
+    })
+    return {
+      nytLists: computed(() => AppState.nytLists)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
