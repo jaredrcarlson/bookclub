af72d2ddf3c39e824534b8e81cc4557b02a0d41d
Becca <becca.vandewater@gmail.com>
2023-08-08 09:49:11 -0600

    book list more reactive

--- bookclub.client/src/pages/ClubListPage.vue
+++ bookclub.client/src/pages/ClubListPage.vue
@@ -64,7 +64,7 @@ export default {
             }
         }
         watchEffect(() => {
-            getBooksByClubId();
+            getBooksByClubId(route.params.clubId);
         });
         return {
             route,
