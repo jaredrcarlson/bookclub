75459b1428bf1f9fc619caca30a523ed939e65e5
Becca <becca.vandewater@gmail.com>
2023-08-10 19:00:42 -0600

    about team card styled

--- bookclub.client/src/components/NYTListCard.vue
+++ bookclub.client/src/components/NYTListCard.vue
@@ -3,8 +3,8 @@
     <section class="row mx-2">
       <h2>{{ l.name }}</h2>
     </section>
-    <section class="dark-blue-bg elevation-3 mx-2 row justify-content-between p-3">
-      <div class="col-md-2 col-12 ghost-bg p-1 pb-3 pb-md-0" v-for="b in l.books" :key="b.rank">
+    <section class="dark-blue-bg elevation-3 rounded mx-2 row justify-content-between p-3">
+      <div class="col-md-2 col-12 ghost-bg p-1 pb-3 pb-md-0 rounded" v-for="b in l.books" :key="b.rank">
           <div class="d-flex align-items-start justify-content-center">
             <img @click="searchGbApi(b.primary_isbn10)" class="img-fluid img-style selectable" :src=b.book_image :alt=b.title>
           </div>
