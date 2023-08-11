aff03744ba5c818b581fecd76869b2790b6a2135
Becca <becca.vandewater@gmail.com>
2023-08-11 12:21:30 -0600

    more styling on nyt

--- bookclub.client/src/components/NYTCard.vue
+++ bookclub.client/src/components/NYTCard.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="container-fluid dark-blue-bg text-light">
-    <section class="row align-items-center">
-      <div class="col-md-2 col-12 my-3">
+    <section class="row">
+      <div class="col-12 mt-3 text-center">
+        <h1>
+          The New York Times Best Sellers
+        </h1>
+      </div>
+    </section>
+    <section class="row">
+      <div class="col-md-3 col-12 my-2 d-flex align-items-center justify-content-center justify-content-md-start">
         <div class="dropdown">
-          <button class="btn orange-btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <button class="btn orange-btn dropdown-toggle ms-md-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
             Choose a best seller list
           </button>
           <ul class="dropdown-menu dropdown-menu-width">
             <li class="px-3 selectable" @click="sortList('Paperback Trade Fiction')">Paperback Trade Fiction</li>
-            <li class="px-3 selectable" @click="sortList('Young Adult Paperback')">Young Adult Paperback</li>
+            <li class="px-3 selectable" @click="sortList('Young Adult Hardcover')">Young Adult Hardcover</li>
             <li class="px-3 selectable" @click="sortList('Graphic Books and Manga')">Graphic Books and Manga</li>
             <li class="px-3 selectable" @click="sortList('Paperback Nonfiction')">Paperback Nonfiction</li>
             <li class="px-3 selectable" @click="sortList('Advice, How-To & Miscellaneous')">Advice, How-To & Miscellaneous</li>
           </ul>
         </div>
-      </div>
-      <div class="col-md-9 col-12 my-3 text-center">
-        <h1>
-          The New York Times Best Sellers
-        </h1>
       </div>
     </section>
     <section class="row justify-content-center">
@@ -26,20 +28,61 @@
         <div id="carouselExample" class="carousel slide">
           <div class="carousel-inner text-center">
             <div class="carousel-item active">
-                <img @click="searchGbApi(activeNytBooks[0].isbn10)" :src=activeNytBooks[0].bookImg class="img-fluid carousel-img selectable" :alt=activeNytBooks[0].title>
+              <div v-if="activeNytBooks[0].isbn10 != 'None'">
+                <img @click="searchGbApi(activeNytBooks[0].isbn10)" :src=activeNytBooks[0].bookImg class="img-fluid carousel-img selectable" role="button" :alt=activeNytBooks[0].title>
+              </div>
+              <div class="d-flex flex-column align-items-center" v-else>
+                <img :src=activeNytBooks[0].bookImg class="img-fluid carousel-img position-relative" :alt=activeNytBooks[0].title style="filter: grayscale(100%);">
+                <p class="bg-danger fw-bold px-5 position-absolute top-50 start-50 translate-middle">
+                  Not Available
+                </p>
+              </div>
             </div>
             <div class="carousel-item">
-                <img @click="searchGbApi(activeNytBooks[1].isbn10)" :src=activeNytBooks[1].bookImg class="img-fluid carousel-img selectable" :alt=activeNytBooks[1].title>
+              <div v-if="activeNytBooks[1].isbn10 != 'None'">
+                <img @click="searchGbApi(activeNytBooks[1].isbn10)" :src=activeNytBooks[1].bookImg class="img-fluid carousel-img selectable" role="button" :alt=activeNytBooks[1].title>
+              </div>
+              <div class="d-flex flex-column align-items-center" v-else>
+                <img :src=activeNytBooks[1].bookImg class="img-fluid carousel-img position-relative" :alt=activeNytBooks[1].title style="filter: grayscale(100%);">
+                <p class="bg-danger fw-bold px-5 position-absolute top-50 start-50 translate-middle">
+                  Not Available
+                </p>
+              </div>
             </div>
             <div class="carousel-item">
-                <img @click="searchGbApi(activeNytBooks[2].isbn10)" :src=activeNytBooks[2].bookImg class="img-fluid carousel-img selectable" :alt=activeNytBooks[2].title>
+              <div v-if="activeNytBooks[2].isbn10 != 'None'">
+                <img @click="searchGbApi(activeNytBooks[2].isbn10)" :src=activeNytBooks[2].bookImg class="img-fluid carousel-img selectable" role="button" :alt=activeNytBooks[2].title>
+              </div>
+              <div class="d-flex flex-column align-items-center" v-else>
+                <img :src=activeNytBooks[2].bookImg class="img-fluid carousel-img position-relative" :alt=activeNytBooks[2].title style="filter: grayscale(100%);">
+                <p class="bg-danger fw-bold px-5 position-absolute top-50 start-50 translate-middle">
+                  Not Available
+                </p>
+              </div>
             </div>
             <div class="carousel-item">
-                <img @click="searchGbApi(activeNytBooks[3].isbn10)" :src=activeNytBooks[3].bookImg class="img-fluid carousel-img selectable" :alt=activeNytBooks[3].title>
+              <div v-if="activeNytBooks[3].isbn10 != 'None'">
+                <img @click="searchGbApi(activeNytBooks[3].isbn10)" :src=activeNytBooks[3].bookImg class="img-fluid carousel-img selectable" role="button" :alt=activeNytBooks[3].title>
+              </div>
+              <div class="d-flex flex-column align-items-center" v-else>
+                <img :src=activeNytBooks[3].bookImg class="img-fluid carousel-img position-relative" :alt=activeNytBooks[3].title style="filter: grayscale(100%);">
+                <p class="bg-danger fw-bold px-5 position-absolute top-50 start-50 translate-middle">
+                  Not Available
+                </p>
+              </div>
             </div>
             <div class="carousel-item">
-                <img @click="searchGbApi(activeNytBooks[4].isbn10)" :src=activeNytBooks[4].bookImg class="img-fluid carousel-img selectable" :alt=activeNytBooks[4].title>
+              <div v-if="activeNytBooks[4].isbn10 != 'None'">
+                <img @click="searchGbApi(activeNytBooks[4].isbn10)" :src=activeNytBooks[4].bookImg class="img-fluid carousel-img selectable" role="button" :alt=activeNytBooks[4].title>
+              </div>
+              <div class="d-flex flex-column align-items-center" v-else>
+                <img :src=activeNytBooks[4].bookImg class="img-fluid carousel-img position-relative" :alt=activeNytBooks[4].title style="filter: grayscale(100%);">
+                <p class="bg-danger fw-bold px-5 position-absolute top-50 start-50 translate-middle">
+                  Not Available
+                </p>
+              </div>
             </div>
+            
           </div>
           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
