a71175ae3633c2f6b5c7375a2689c77bd43c77aa
Jared Carlson <jaredrcarlson@gmail.com>
2023-09-17 06:17:54 -0600

    api base update

--- bookclub.client/src/env.js
+++ bookclub.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : 'bookclub'
+export const baseURL = dev ? 'http://localhost:3000' : 'https://jaredcarlson.dev/bookclub'
 export const useSockets = false
 export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
 export const audience = 'https://localhost:8888'
